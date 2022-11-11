85747bf518d4408956dfe9df07a6cdc296244069
jhedemann <117086556+jhedemann@users.noreply.github.com>
2022-11-11 09:37:42 +0100

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -2,19 +2,18 @@ import numpy as np
 from numpy.random import default_rng
 import tensorflow as tf
 
-'''
-# Task 01 - Building our data set
 
+# Task 01 - Building our data set
+'''
 # our input values x
 x = np.array(default_rng(42).random((1,100)))
 
 print(x)
 
 #our targets t
-t = x**3 - x**2
-
-print(t)
+t = x**2
 '''
+
 # Task 02 - Perceptrons
 
 
@@ -37,58 +36,55 @@ class Layer:
         self.activation = np.maximum(0, self.preactivation)
         return self.activation
 
-    def backward_step(self, targets):
-        diff = self.activation - targets
-        #print("{} = {}".format("diff", diff))
-        diff_squared = diff**2
-        #print("{} = {}".format("diff_squared", diff_squared))
-        loss = diff_squared.mean()
-        #print("{} = {}".format("loss", loss))
-        gradients_weights = tf.transpose(self.input)*(tf.nn.sigmoid(self.preactivation) * (1 - tf.nn.sigmoid(self.preactivation))) * loss
+    def backward_step(self, dLda):
+        deriv_preactivation = [1 if self.preactivation[i] > 0 else 0 for i in self.preactivation]
+        gradients_weights = tf.transpose(self.input) * deriv_preactivation * dLda
         #print(gradients_weights)
-        gradients_biases = tf.nn.sigmoid(self.preactivation) * (1 - tf.nn.sigmoid(self.preactivation)) * loss
+        gradients_biases = (self.preactivation > 0) * dLda
         #print(gradients_biases)
-        gradients_input = gradients_biases * tf.transpose(self.weights)
-        #print(gradients_input)
         #print(self.weights)
         self.weights = self.weights - (0.03 * gradients_weights)
         #print(self.weights)
         #print(self.biases)
         self.biases = self.biases - (0.03 * gradients_biases)
         #print(self.biases)
-        return
+        return dLda * (self.preactivation > 0) * tf.transpose(self.weights)
 
 class MLP:
 
     def __init__(self, layers):
         self.layers = np.array(layers)
         self.output = []
+        
 
-    def feed_forward(self, input):
-        self.layers[0].forward_step(input)
-        print(self.layers[0].activation)
-        for i in range(1, len(self.layers)-1):
-            self.layers[i+1].forward_step(self.layers[i].activation)
-            print(self.layers[i+1].activation)
+    def forward_step(self, input):
+        print(input)
+        for i in range(0, len(self.layers)-1):
+            print(self.layers[i].weights)
+            if i == 0:
+                self.layers[i].forward_step(input)
+            else:
+                self.layers[i].forward_step(self.layers[i-1].activation)
+            print(self.layers[i].activation)
+        print(self.layers[-1].activation)
         return self.layers[-1].activation
 
-    def backpropagation(self, loss):
-        
+    def backpropagation(self, dLda):
+        for layer in np.flip(self.layers):
+            dLda = layer.backward_step(dLda)
+            #print(layer.weights)
+            #print(layer.biases)
         return
 
 
 
-layer1 = Layer(3, 1)
-layer2 = Layer(3, 3)
-layer3 = Layer(3, 3)
-layer4 = Layer(1, 3)
+layer1 = Layer(10, 1)
+layer2 = Layer(1, 10)
 
-mlp = MLP([layer1, layer2, layer3, layer4])
-mlp.feed_forward(4)
 
+mlp = MLP([layer1, layer2])
+#mlp.forward_step(4)
+mlp.backpropagation(mlp.forward_step(4) - 16)
 #layer1.forward_step(np.array([2, 3, 4]))
 
 #targets = [1, 4, 5]
-
-#layer1.backward_step(targets)
-
