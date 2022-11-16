2c84e59369703313ccc059da8869ca3c6f75abd5
dyanita23 <116731478+dyanita23@users.noreply.github.com>
2022-11-16 13:01:47 +0100

    Update tensorflow03.py

--- tensorflow03.py
+++ tensorflow03.py
@@ -98,3 +98,33 @@ for epoch in range(num_epochs):
     test_losses.append(test_loss)
     test_accuracies.append(test_accuracy)
     
+
+# Visualization
+
+def visualization ( train_losses , train_accuracies , test_losses ,
+test_accuracies ):
+ """ Visualizes accuracy and loss for training and test data using
+the mean of each epoch .
+Loss is displayed in a regular line , accuracy in a dotted
+line .
+Training data is displayed in blue , test data in red .
+Parameters
+----------
+train_losses : numpy . ndarray
+training losses
+train_accuracies : numpy . ndarray
+training accuracies
+test_losses : numpy . ndarray
+test losses
+test_accuracies : numpy . ndarray
+test accuracies
+"""
+plt . figure ()
+line1 , = plt . plot ( train_losses , "b-")
+line2 , = plt . plot ( test_losses , "r-")
+line3 , = plt . plot ( train_accuracies , "b:")
+line4 , = plt . plot ( test_accuracies , "r:")
+plt . xlabel (" Training steps ")
+plt . ylabel (" Loss / Accuracy ")
+plt . legend (( line1 , line2 , line3 , line4 ) , (" training loss ", " testloss ", " train accuracy ", " test accuracy "))
+plt . show ()
